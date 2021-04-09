94e4bb3df09ef39a6157496a7ece23b89d4f8cd0
Rimmen17 <82200662+Rimmen17@users.noreply.github.com>
2021-04-09 08:33:13 +0200

    Add files via upload

---
+++ Code_lock_2_den_her.ino
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int incomingByte = 0;
+ char arr[] = {3,7,12};
+  
+  void setup() {
+  Serial.begin(9600);
+  }
+
+void loop() {
+if (Serial.available() >0)
+  incomingByte = Serial.read();
+
+  if(arr[0]==incomingByte=48)
+    // blink ok / 1 gang)
+    if (arr[2] == incomingByte=48)
+      //blink 2 gange
+    if (arr[2] == incomingByte=48)
+      //åben dør
+ else
+ // blink hurtigt rød
+
+Serial.print("I received");
+Serial.println (incomingByte, DEC);
+}
